a5c6ac97656c1cf02841cc5351cb228d955681d3
Leon <leonmorin410@gmail.com>
2023-06-13 02:52:30 +0100

    Refactor the basic logic for the game and added some error checking.

--- venv/Main.py
+++ venv/Main.py
@@ -5,34 +5,41 @@ while True:
     try:
         upper_bound = int(input("Input an upperbound for the number generated: "))
         attempts = int(input("Input the maximum number of attempts: "))
-        break
+        if upper_bound < 0 or attempts < 0:
+            print("Input needs to be greater than 0.")
+        else:
+            break
     except ValueError:
         print("Invalid input. Please enter valid integers.")
-# generate a random number within range
+# generate a random number within ranges
 goal = random.randint(0, upper_bound)
-gameover = False
 
-while(attempts>0):
-    # take user input guess
-    while True:
-        try:
-            guess = int(input("Enter Guess: "))
-            break
-        except ValueError:
-            print("Input an Integer guess.")
-    # check the players guess against generated num
-    # if not ==,  attempt -1 and tell user whether guess is too high or too low
-    if(guess != goal):
-        if(guess > goal):
-            print("Too high")
+def game_run(goal, attempts, upper):
+    while (attempts > 0):
+        # take user input guess
+        while True:
+            try:
+                guess = int(input("Enter Guess: "))
+                if guess > upper or guess < 0:
+                    print("Enter a guess lower than the upperbound")
+                else:
+                    break
+            except ValueError:
+                print("Input an Integer guess.")
+        # check the players guess against generated num
+        # if not ==,  attempt -1 and tell user whether guess is too high or too low
+        if (guess != goal):
+            if (guess > goal):
+                print("Too high")
+            else:
+                print("Too low")
+            attempts = attempts - 1
+            print(attempts)
         else:
-            print("Too low")
-        attempts= attempts-1
-        print(attempts)
-    else:
-    # if guess is correct end game
-        print("You win!! Remaining attempts: " , attempts)
-        break
+            # if guess is correct end game
+            return "You win!! Remaining attempts: " + str(attempts)
+    return "You lost!! The number was " + str(goal)
+
 
 # attempts = 0 end game
-# print("You lost! The number was ", goal)
+print(game_run(goal, attempts, upper_bound))
